aec98d2c2368e8c7d17b735505c55fab5d6c2882
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-03-27 09:49:47 +0200

    l

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -20,7 +20,7 @@ router.post("/admin", async (req, res) => {
         const token =  jwt.sign(
             {id: admin._id, username: admin.username, role: admin.role}, 
             JWT_SECRET,
-            {expiresIn: "1h"}
+            {expiresIn: "24h"}
         )
 
         return res.status(200).json({
